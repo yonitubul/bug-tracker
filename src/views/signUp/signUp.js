ff1d903ef2dcaf0e964c8569a36151abf75887a5
Yoni Tubul <yonitubul137@gmail.com>
2022-01-21 00:30:53 +0200

    :sparkles: Firebase authentication setup and signup form created

---
+++ src/views/signUp/signUp.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import {Card, Form, Button} from 'react-bootstrap'
+
+export default function signUp() {
+  return (
+  <>
+    <Card>
+    <Card.body>
+        <h2 className="text-center mb-4">Sign Up</h2>
+        <Form>
+        <Form.Group id="email">
+
+        </Form.Group>
+
+        </Form>
+    </Card.body>
+
+    </Card>
+    <di className="w-100 text-center mt-2">
+        Have an account? Log In here
+    </di>
+  </>
+  )
+}
